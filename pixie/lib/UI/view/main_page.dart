af69ec5b70688537d07b83ae0fcc375343928bba
talalcodes55 <talalalghazal55@gmail.com>
2025-01-21 15:16:27 +0300

    created the main app's interface

---
+++ pixie/lib/UI/view/main_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:pixie/UI/components/navbar.dart';
+import 'package:pixie/UI/view/favorites_page.dart';
+import 'package:pixie/UI/view/home_page.dart';
+import 'package:pixie/UI/view/profile_page.dart';
+import 'package:pixie/UI/view/search_page.dart';
+import 'package:pixie/controllers/main_controller.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int navIndex = 0;
+  List<Widget> pages = const [
+    HomePage(),
+    FavoritesPage(),
+    SearchPage(),
+    ProfilePage()
+  ];
+  @override
+  Widget build(BuildContext context) {
+    MainController mainController = Get.put(MainController());
+    return Scaffold(
+      body: PageView(
+          controller: mainController.pageController,
+          onPageChanged: (value) => mainController.onPageChanged(value),
+          children: pages),
+      bottomNavigationBar: const Navbar(),
+    );
+  }
+}
