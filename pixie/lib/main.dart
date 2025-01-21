a0b4d36350c534c7b1774f3e3eecde443244a270
talalcodes55 <talalalghazal55@gmail.com>
2025-01-21 15:14:08 +0300

    added GetX to the project

--- pixie/lib/main.dart
+++ pixie/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pixie/UI/view/main_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/mainPage',
+      getPages: [
+        GetPage(name: '/mainPage', page: () => const MainPage(),),
+      ],
+      // home: const HomePage(),
     );
   }
 }
