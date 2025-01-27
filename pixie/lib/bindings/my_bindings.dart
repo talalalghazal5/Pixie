5d9fd2dc44086009c7decaa7ae435d71891058a7
talalcodes55 <talalalghazal55@gmail.com>
2025-01-27 21:25:00 +0300

    initialized the bindings for the app and created a loading page

---
+++ pixie/lib/bindings/my_bindings.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+import 'package:pixie/controllers/main_controller.dart';
+import 'package:pixie/controllers/photos_controller.dart';
+
+class MyBindings extends Bindings {
+  @override
+  void dependencies() {
+    Get.put(PhotosController());
+    Get.put(MainController());
+    Get.put(MainController());
+  }
+}
--- pixie/lib/main.dart
+++ pixie/lib/main.dart
@@ -2,7 +2,9 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:pixie/UI/view/loading_page.dart';
 import 'package:pixie/UI/view/main_page.dart';
+import 'package:pixie/bindings/my_bindings.dart';
 import 'package:pixie/controllers/photos_controller.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -11,13 +13,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   preferences = await SharedPreferences.getInstance();
   runApp(const MainApp());
-  PhotosController photosController =
-      Get.put<PhotosController>(PhotosController());
-  try {
-    photosController.loadPhotos();
-  } on SocketException {
-    rethrow;
-  }
 }
 
 class MainApp extends StatelessWidget {
@@ -27,11 +22,16 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/mainPage',
+      initialBinding: MyBindings(),
+      initialRoute: '/loadingPage',
       getPages: [
         GetPage(
           name: '/mainPage',
           page: () => const MainPage(),
+        ),
+        GetPage(
+          name: '/loadingPage',
+          page: () => const LoadingPage(),
         ),
       ],
       // home: const HomePage(),
