c9b518fc3d3415a98bbec3093bc0e3b93e753eb4
talalcodes55 <talalalghazal55@gmail.com>
2025-01-31 16:38:21 +0300

    declared scaffold key

--- pixie/lib/main.dart
+++ pixie/lib/main.dart
@@ -1,14 +1,15 @@
-import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pixie/UI/view/loading_page.dart';
 import 'package:pixie/UI/view/main_page.dart';
+import 'package:pixie/bindings/my_api_key.dart';
 import 'package:pixie/bindings/my_bindings.dart';
-import 'package:pixie/controllers/photos_controller.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 late SharedPreferences preferences;
+final String apiKey = MyApiKey.apiKey;
+final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   preferences = await SharedPreferences.getInstance();
