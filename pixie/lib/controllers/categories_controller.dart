041d31550efd8d9a4276a4f197d8505c15227332
talalcodes55 <talalalghazal55@gmail.com>
2025-04-04 22:23:17 +0300

    applied localization to categories lists

--- pixie/lib/controllers/categories_controller.dart
+++ pixie/lib/controllers/categories_controller.dart
@@ -1,20 +1,23 @@
+import 'package:get/get.dart';
+import 'package:pixie/bindings/my_bindings.dart';
+
 class CategoriesController {
   static const categoriesAssetsUrl = 'assets/images/categories';
   static const colorsAssetsUrl = 'assets/images/colors';
   static List<Map<String, String>> categories = [
-    {'name' : 'Animals', 'imagePath' : '$categoriesAssetsUrl/animals.jpg'},
-    {'name' : 'Nature', 'imagePath' : '$categoriesAssetsUrl/nature.jpg'},
-    {'name' : 'Food', 'imagePath' : '$categoriesAssetsUrl/food.jpg'},
-    {'name' : 'Architecture', 'imagePath' : '$categoriesAssetsUrl/architecture.jpg'},
-    {'name' : 'Minimal', 'imagePath' : '$categoriesAssetsUrl/minimal.jpg'},
-    {'name' : 'Space', 'imagePath' : '$categoriesAssetsUrl/space.jpg'},
+    {'name' : 'animalsCategoryCard'.tr, 'imagePath' : '$categoriesAssetsUrl/animals.jpg'},
+    {'name' : 'natureCategoryCard'.tr, 'imagePath' : '$categoriesAssetsUrl/nature.jpg'},
+    {'name' : 'foodCategoryCard'.tr, 'imagePath' : '$categoriesAssetsUrl/food.jpg'},
+    {'name' : 'architectureCategoryCard'.tr, 'imagePath' : '$categoriesAssetsUrl/architecture.jpg'},
+    {'name' : 'minimalCategoryCard'.tr, 'imagePath' : '$categoriesAssetsUrl/minimal.jpg'},
+    {'name' : 'spaceCategoryCard'.tr, 'imagePath' : '$categoriesAssetsUrl/space.jpg'},
   ];
   static List<Map<String, String>> colors = [
-    {'name' : 'Blue', 'imagePath' : '$colorsAssetsUrl/blue.jpg'},
-    {'name' : 'Red', 'imagePath' : '$colorsAssetsUrl/red.jpg'},
-    {'name' : 'Green', 'imagePath' : '$colorsAssetsUrl/green.jpg'},
-    {'name' : 'Brown', 'imagePath' : '$colorsAssetsUrl/brown.jpg'},
-    {'name' : 'Black', 'imagePath' : '$colorsAssetsUrl/black.jpg'},
-    {'name' : 'White', 'imagePath' : '$colorsAssetsUrl/white.jpg'},
+    {'name' : 'blueCategoryCard'.tr, 'imagePath' : '$colorsAssetsUrl/blue.jpg'},
+    {'name' : 'redCategoryCard'.tr, 'imagePath' : '$colorsAssetsUrl/red.jpg'},
+    {'name' : 'greenCategoryCard'.tr, 'imagePath' : '$colorsAssetsUrl/green.jpg'},
+    {'name' : 'brownCategoryCard', 'imagePath' : '$colorsAssetsUrl/brown.jpg'},
+    {'name' : 'blackCategoryCard', 'imagePath' : '$colorsAssetsUrl/black.jpg'},
+    {'name' : 'whiteCategoryCard', 'imagePath' : '$colorsAssetsUrl/white.jpg'},
   ];
 }
